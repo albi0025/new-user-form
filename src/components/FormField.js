b6aac18e6639a5e1233c3b6b5565097fda437855
Jennifer Phelps <albi0025@gmail.com>
2017-03-04 15:57:36 -0700

    added props to form field component

--- src/components/FormField.js
+++ src/components/FormField.js
@@ -2,20 +2,24 @@ import React from 'react';
 
 class FormField extends React.Component {
 
-  constructor() {
-    super();
-  }
+  // constructor() {
+  //   super();
+  // }
 
   render() {
     return (
       <div>
-        <label htmlFor="username">User Name:</label>
-        <input type="text" id="username" name="username" />
+        <label htmlFor={this.props.id}>{this.props.label}</label>
+        <input type="text" id={this.props.id} name={this.props.id} />
         <p>validation</p>
       </div>
     );
   }
 }
 
+FormField.propTypes = {
+  label: React.PropTypes.string.isRequired,
+  id: React.PropTypes.string.isRequired
+};
 
 export default FormField;
--- src/components/NewUserForm.js
+++ src/components/NewUserForm.js
@@ -3,17 +3,17 @@ import FormField from './FormField';
 
 class NewUserForm extends React.Component {
 
-  constructor() {
-    super();
-  }
+  // constructor() {
+  //   super();
+  // }
 
   render() {
     return (
       <div>
         <div><h1>Error!</h1></div>
         <form>
-          <FormField />
-          <FormField />
+          <FormField id="username" label="Username:"/>
+          <FormField id="password" label="Password:"/>
           <input type="submit" value="Submit" />
         </form>
       </div>

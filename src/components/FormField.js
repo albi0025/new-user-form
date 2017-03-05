2d3b5ccce331155e0fba23f9fff9fad818451039
Jennifer Phelps <albi0025@gmail.com>
2017-03-04 17:17:48 -0700

    username validation function

--- src/components/FormField.js
+++ src/components/FormField.js
@@ -9,8 +9,8 @@ class FormField extends React.Component {
   render() {
     return (
       <div>
-        <label htmlFor={this.props.id}>{this.props.label}</label>
-        <input type="text" id={this.props.id} name={this.props.id} />
+        <label htmlFor={this.props.id}>{this.props.label}</label><br/>
+        <input onChange={this.props.validation} type="text" id={this.props.id} name={this.props.id} />
         <p>validation</p>
       </div>
     );
@@ -19,7 +19,8 @@ class FormField extends React.Component {
 
 FormField.propTypes = {
   label: React.PropTypes.string.isRequired,
-  id: React.PropTypes.string.isRequired
+  id: React.PropTypes.string.isRequired,
+  validation: React.PropTypes.func
 };
 
 export default FormField;
--- src/components/NewUserForm.js
+++ src/components/NewUserForm.js
@@ -7,12 +7,28 @@ class NewUserForm extends React.Component {
   //   super();
   // }
 
+  userNameValidation(e) {
+    var str = e.target.value
+    var splitByAt = str.split("@");
+    if(splitByAt.length !== 2) {
+      console.log("invalid")
+      return false;
+    }
+    var splitByPeriod = splitByAt[1].split(".");
+    if(splitByPeriod.length !== 2 ) {
+      console.log("invalid")
+      return false;
+    }
+    console.log("valid")
+    return true;
+  }
+
   render() {
     return (
       <div>
         <div><h1>Error!</h1></div>
         <form>
-          <FormField id="username" label="Username:"/>
+          <FormField validation={this.userNameValidation} id="username" label="Username:"/>
           <FormField id="password" label="Password:"/>
           <input type="submit" value="Submit" />
         </form>

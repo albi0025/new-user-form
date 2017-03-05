1037a15d3b73a9122fc8909db1d3a526ed847ac8
Jennifer Phelps <albi0025@gmail.com>
2017-03-05 12:28:54 -0700

    added state on form field component and toggled the display of error text based on the state

--- src/components/FormField.js
+++ src/components/FormField.js
@@ -2,13 +2,36 @@ import React from 'react';
 
 class FormField extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      valid: false
+    };
+    this.handleValueChange = this.handleValueChange.bind(this);
+  }
+
+  handleValueChange(e) {
+    let validationResult = this.props.validation(e);
+    this.setState({ valid: validationResult });
+    this.toggleErrorTextVisibility();
+  }
+
+  toggleErrorTextVisibility() {
+    let errorTextElement = document.getElementById(this.props.id + "-error-text");
+    if(this.state.valid) {
+      errorTextElement.style.visibility = "hidden";
+    } else {
+      errorTextElement.style.visibility = "visible";
+    }
+  }
+
   render() {
     return (
       <div>
         <label htmlFor={this.props.id}>{this.props.label}</label><br/>
-        <input onChange={this.props.validation} type="text"
+        <input onChange={this.handleValueChange} type="text"
                id={this.props.id} name={this.props.id} />
-        <p>{this.props.errorText}</p>
+        <p id={this.props.id + "-error-text"}>{this.props.errorText}</p>
       </div>
     );
   }
